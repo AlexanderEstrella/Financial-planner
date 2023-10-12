1c2997c70eb9276b2596947a1846a517157dc4f0
AlexanderME <alexanderestrella23@gmail.com>
2023-10-12 14:38:54 -0400

    number format set up

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import moment from "moment";
 const Home = () => {
-  return <h1>Hello </h1>;
+  const [income, setIncome] = useState("");
+
+  let month = moment().format("MMM Do YY");
+  const updatedValue = (value) => {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(value);
+  };
+
+  const handleIncomeChange = (e) => {
+    setIncome(e.target.value);
+  };
+  return (
+    <div className="Fcontainer">
+      <h1 className="Date">{month}</h1>
+      <div>
+        <label>Net Income:</label>
+        <span>
+          <input
+            type="text"
+            className="dollar"
+            value={income}
+            onChange={handleIncomeChange}
+          ></input>
+        </span>
+      </div>
+      <div>
+        <label>Formatted Income:</label>
+        <span>{updatedValue(income)}</span>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
